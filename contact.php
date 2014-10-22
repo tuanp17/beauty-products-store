ad035c18832feaa835020c3e681acb7325ae70af
TUAN PHAM <tuanp1711@gmail.com>
2014-10-21 23:22:09 -0400

    add contact and message boxes

--- contact.php
+++ contact.php
@@ -1,6 +1,64 @@
 <?php include("inc/header.php"); ?>
 	<div class="container">
-		
-		
+		<hr>
+		<ol class="breadcrumb">
+  		<li><a href="index.php">Trang Chủ</a></li>
+  		<li class="active">Liên Hệ</a></li>
+		</ol>	
+		<div class="row">
+			<div class="col-xs-9 col-sm-5">
+				<div class="panel panel-success">
+					<div class="panel-heading">
+  					<h3 class="panel-title"><img src="img/phone-square.png">&nbsp;Liên Hệ Sĩ Lẻ Mỹ Phẩm</h3>
+					</div>
+					<div class="panel-body panel-contact">
+  					<h4><strong>VY VY Boutique</strong><br></h4>
+  					<i>Địa Chỉ:</i> 7 Phan Chu Trinh-P.Xương Huân-Nha Trang-Khánh Hòa<br>
+  					<i>Điện Thoại:</i> 01687520700<br>
+  					<i>Email:</i> <a href="mailto:teresangocvy@gmail.com">teresangocvy@gmail.com</a>
+  					<ul class="icon-bar-contact">
+  						<li><a href="https://www.facebook.com/VYVYBoutique.beautiful?ref=br_rs"><img src="img/facebook-icon.png" alt="facebook-icon"></a></li>
+  						<li><a href="#"><img src="img/zalo-icon.jpg" alt="zalo-icon"></a></li>
+  						<li><a href="#"><img src="img/viber-icon.png" alt="viber-icon"></a></li>
+  					</ul>
+					</div><!-- End panel body -->
+				</div><!-- End Panel heading -->
+			</div><!-- End first column -->
+
+			<div class="col-xs-9 col-sm-5">
+			 	<div class="panel panel-success">
+			 		<div class="panel-heading">
+			 			<h3 class="panel-title"><img src="img/comments.png" alt="comments-icon">&nbsp;Phản Hồi - Góp Ý</h3>
+			 		</div>
+			 		<div class="panel-body">
+			 			<form class="form-horizontal" role="form">
+			 				<div class="form-group">
+			 					<label for="inputName" class="col-sm-3 control-label">Họ Tên:</label>
+			 					<div class="col-sm-9">
+			 						<input type="name" class="form-control" id="inputName" placeholder="Họ Tên">
+			 					</div>
+			 				</div>
+			 				<div class="form-group">
+			 					<label for="inputEmail" class="col-sm-3 control-label">Email:</label>
+			 					<div class="col-sm-9">
+			 						<input type="email" class="form-control" id="inputEmail" placeholder="Email">
+			 					</div>
+			 				</div>
+			 				<div class="form-group">
+			 					<label for="inputMessage" class="col-sm-3 control-label">Tin Nhắn:</label>
+			 					<div class="col-sm-9">
+			 						<textarea class="form-control" rows="2" placeholder="Giữ liên lạc với tôi nhé!"></textarea>
+			 					</div>
+			 				</div>
+			 			</form>
+			 		</div>
+			 	</div>
+			</div>
+			<div class="col-xs-3 col-sm-2">
+				<div class="container-fluid">
+					tuan
+				</div>
+			</div>
+		</div>
 	</div>
 <?php include("inc/footer.php"); ?>
