929fe35b230aa872d08e232687f918b4054e011d
TUAN PHAM <tuanp1711@gmail.com>
2014-10-23 11:10:57 -0400

    pop products page

---
+++ popular_products.php
@@ -0,0 +1,36 @@
+<?php
+	$page_title = "Sản Phẩm Nổi Bật | VYVY Boutique";
+	include('inc/header.php'); 
+?>
+	<div class="container">
+		<ol class="breadcrumb">
+  			<li><a href="index.php">Trang Chủ</a></li>
+  			<li><a href="index.php">Chăm Sóc</a></li>
+  			<li class="active">Nổi Bật</li>
+		</ol>
+
+		<div class="panel panel-success panel-custom">
+			<div class="panel-heading">
+				<h3 class="panel-title">
+					<img src="img/thumbs-up.png" alt="popular-products-logo">&nbsp;&nbsp;Sản Phẩm Nổi Bật  
+        </h3>
+			</div>
+
+			<div class="panel-body">
+				<div class="row">
+  				<div class="col-sm-4 col-md-3">
+    				<div class="thumbnail">
+      				<img data-src="holder.js/250x250" src="img/pop1.png" alt="product_pop_1">
+      				<div class="caption">
+        				<strong>Pure White Pro - Dưỡng Trắng, Trị Nám, Tàn Nhang</strong>
+        				<p><a href="#" class="btn btn-success" role="button">Mua</a></p>
+      				</div>
+    				</div>
+  				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php
+	include('inc/footer.php'); 
+?>
