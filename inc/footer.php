27fdea2ec714b35e75df25903a519bdad0dd5782
TUAN PHAM <tuanp1711@gmail.com>
2014-10-23 11:10:10 -0400

    add links

--- inc/footer.php
+++ inc/footer.php
@@ -4,9 +4,9 @@
         <div class = "row footer">
           <div class="col-xs-6 col-sm-4">
             <h4><a href="index.php"><strong>VY VY Boutique</strong></a></h4>
-            <p><a href ="#"><img src = "img/location.png" alt = "location">7 Phan Chu Trinh - P.Xương Huân</br> Nha Trang - Khánh Hòa</a></p>
+            <p><a href ="contact.php"><img src = "img/location.png" alt = "location">7 Phan Chu Trinh - P.Xương Huân</br> Nha Trang - Khánh Hòa</a></p>
             <p>Điện Thoại: 01687520700</p>
-            <p><span class="glyphicon glyphicon-copyright-mark"></span>&nbsp;<?php echo date("Y") ?> VyVyBoutique</p>
+            <p><span class="glyphicon glyphicon-copyright-mark"></span>&nbsp;<?php echo date("Y");?> VyVyBoutique</p>
           </div>
           <div class="col-xs-6 col-sm-4">
             <ul class="footer-nav">
@@ -14,8 +14,10 @@
               <li class ="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sản Phẩm&nbsp;<span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
-                  <li><a href="#">Chăm Sóc Da Mặt</a></li>
-                  <li><a href="#">Chăm Sóc Cơ Thể</a></li>
+                  <li><a href="popular_products.php">Nổi Bật</a></li>
+                  <li><a href="newest_products.php">Mới Nhất</a></li>
+                  <li><a href="face_products.php">Dành Cho Da Mặt</a></li>
+                  <li><a href="body_products.php">Dành Cho Body</a></li>
                 </ul>
               </li>
               <li><a href="contact.php">Liên Hệ</a></li>
