e0516859b7cb348a8f04bdad33976a7337f7b61f
TUAN PHAM <tuanp1711@gmail.com>
2014-10-28 11:49:00 -0400

    add carousel

--- index.php
+++ index.php
@@ -138,4 +138,129 @@ include("inc/popular_products_inc.php");
     </div><!-- End container -->
     <!-- End second-carousel-content -->
     <!-- End Content -->
+    <div class="container">
+      <div class="panel panel-success panel-custom">
+        <div class="panel-heading">
+          <h3 class="panel-title"><img src="img/smile-o.png" alt="face-logo">&nbsp;&nbsp;Chăm Sóc Da Mặt
+            <p class="pull-right"><a href="face_products.php">Xem thêm&nbsp;<img src="img/forward.png" alt="forward-icon">
+            </a></p>
+          </h3>
+          
+        </div>
+        <div class="panel-body">
+          <div class="col-md-12">
+            <div class="well-none">
+              <div id="face-products-carousel" class="carousel slide">      
+                <!-- Carousel items -->
+                <div class="carousel-inner">
+                  <div class="item active">
+                    <div class="row">
+                        <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop1.png" alt="Image" class="img-responsive"></a>
+                        </div>
+                        <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop2.png" alt="Image" class="img-responsive"></a>
+                        </div>
+                        <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop3.png" alt="Image" class="img-responsive"></a>
+                        </div>
+                        <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop4.png" alt="Image" class="img-responsive"></a>
+                        </div>
+                    </div>
+                  <!--/row-->
+                  </div>
+                  <!--/item-->
+                  <div class="item">
+                    <div class="row">
+                      <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop1.png" alt="Image" class="img-responsive"></a>
+                      </div>
+                      <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop1.png" alt="Image" class="img-responsive"></a>
+                      </div>
+                      <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop1.png" alt="Image" class="img-responsive"></a>
+                      </div>
+                      <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop1.png" alt="Image" class="img-responsive"></a>
+                      </div>
+                    </div>
+                  <!--/row-->
+                  </div>
+                  <!--/item-->
+                </div>
+                <!--/carousel-inner--> 
+                <a class="left carousel-control" href="#face-products-carousel" data-slide="prev">
+                  <span class="glyphicon glyphicon-circle-arrow-left"></span>
+                </a>
+                <a class="right carousel-control" href="#face-products-carousel" data-slide="next">
+                  <span class="glyphicon glyphicon-circle-arrow-right"></span>
+                </a>
+              </div>
+              <!--/myCarousel-->
+            </div>
+            <!--/well-->
+          </div>
+        </div><!-- End Panel body -->
+      </div><!-- End Panel -->
+
+    </div><!-- End container -->
+    <!-- End second-carousel-content -->
+    <!-- End Content -->
+
+    <div class="container">
+      <div class="panel panel-success panel-custom">
+        <div class="panel-heading">
+          <h3 class="panel-title"><img src="img/child.png" alt="face-logo">&nbsp;&nbsp;Chăm Sóc Cơ Thể
+            <p class="pull-right"><a href="body_products.php">Xem thêm&nbsp;<img src="img/forward.png" alt="forward-icon">
+            </a></p>
+          </h3>        
+        </div>
+        <div class="panel-body">
+          <div class="col-md-12">
+            <div class="well-none">
+              <div id="body-products-carousel" class="carousel slide">      
+                <!-- Carousel items -->
+                <div class="carousel-inner">
+                  <div class="item active">
+                    <div class="row">
+                        <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop1.png" alt="Image" class="img-responsive"></a>
+                        </div>
+                        <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop2.png" alt="Image" class="img-responsive"></a>
+                        </div>
+                        <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop3.png" alt="Image" class="img-responsive"></a>
+                        </div>
+                        <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop4.png" alt="Image" class="img-responsive"></a>
+                        </div>
+                    </div>
+                  <!--/row-->
+                  </div>
+                  <!--/item-->
+                  <div class="item">
+                    <div class="row">
+                      <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop1.png" alt="Image" class="img-responsive"></a>
+                      </div>
+                      <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop1.png" alt="Image" class="img-responsive"></a>
+                      </div>
+                      <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop1.png" alt="Image" class="img-responsive"></a>
+                      </div>
+                      <div class="col-sm-3"><a href="#x" class="thumbnail"><img src="img/pop1.png" alt="Image" class="img-responsive"></a>
+                      </div>
+                    </div>
+                  <!--/row-->
+                  </div>
+                  <!--/item-->
+                </div>
+                <!--/carousel-inner--> 
+                <a class="left carousel-control" href="#body-products-carousel" data-slide="prev">
+                  <span class="glyphicon glyphicon-circle-arrow-left"></span>
+                </a>
+                <a class="right carousel-control" href="#body-products-carousel" data-slide="next">
+                  <span class="glyphicon glyphicon-circle-arrow-right"></span>
+                </a>
+              </div>
+              <!--/myCarousel-->
+            </div>
+            <!--/well-->
+          </div>
+        </div><!-- End Panel body -->
+      </div><!-- End Panel -->
+
+    </div><!-- End container -->
+    <!-- End second-carousel-content -->
+    <!-- End Content -->
+    
 <?php include("inc/footer.php"); ?>
