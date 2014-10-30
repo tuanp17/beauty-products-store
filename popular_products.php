ca59866567688dede1c35e57e6c9fe39e76e2c5c
TUAN PHAM <tuanp1711@gmail.com>
2014-10-30 00:18:02 -0400

    add link

--- popular_products.php
+++ popular_products.php
@@ -25,7 +25,7 @@
 	      				<img data-src="holder.js/250x250" src="<?php echo $pop_product["img"];?>" alt="<?php echo $pop_product["name"];?>">
 	      				<div class="caption">
 	        				<p><a href="popular_product_details.php?id=<?php echo $pop_product_id; ?>" class="thumbnail-caption"><strong><?php echo $pop_product["name"];?></strong></a></p>
-	        				<p><a href="#" class="btn btn-success" role="button">Xem chi tiết</a></p>
+	        				<p><a href="popular_product_details.php?id=<?php echo $pop_product_id; ?>" class="btn btn-success" role="button">Xem chi tiết</a></p>
 	      				</div>
 	    				</div>
 	  				</div>
