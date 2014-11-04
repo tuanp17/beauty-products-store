099c3598f6d6bc993ad0dbf1082c687a10cc70d2
TUAN PHAM <tuanp1711@gmail.com>
2014-11-04 12:31:24 -0500

    add products

--- popular_products/popular_products.php
+++ popular_products/popular_products.php
@@ -1,10 +1,9 @@
 <?php 
 require_once('../inc/initialize.php');
 include(ROOT_PATH . 'inc/popular_products_inc.php'); 
-?>
-<?php
-	$page_title = "Sản Phẩm Nổi Bật | VYVY Boutique";
-	include(ROOT_PATH . 'inc/header.php'); 
+
+$page_title = "Sản Phẩm Nổi Bật | VYVY Boutique";
+include(ROOT_PATH . 'inc/header.php'); 
 ?>
 	<div class="container">
 		<ol class="breadcrumb">
@@ -23,14 +22,14 @@ include(ROOT_PATH . 'inc/popular_products_inc.php');
 			<div class="panel-body">
 				<div class="row">
 					<?php 
-					 	$total_pop_products = count($pop_products);
-					 	$position = 0;
+						$total_pop_products = count($pop_products);
+            $position = 0;
             $display_products_pop_page = "";
-						foreach ($pop_products as $pop_product_id => $pop_product) { 
+						foreach ($pop_products as $pop_product) {
 							$position += 1;
-							if ($total_pop_products - $position < 8) { 
-								$display_products_pop_page = display_products_pop_page($pop_product_id, $pop_product).$display_products_pop_page;
-							}
+              if ($total_pop_products - $position < 8) { 
+							$display_products_pop_page = display_products_pop_page($pop_product).$display_products_pop_page;	
+							}					
   					} 
   					echo $display_products_pop_page;
   				?>
