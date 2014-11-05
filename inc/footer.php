882e05cbd115fab273b6af113e84ece05868c0ad
TUAN PHAM <tuanp1711@gmail.com>
2014-11-04 22:33:15 -0500

    js error fix

--- inc/footer.php
+++ inc/footer.php
@@ -34,6 +34,6 @@
     </footer>
     <!-- End Footer -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>js/bootstrap.min.js"></script>
   </body>
 </html>
