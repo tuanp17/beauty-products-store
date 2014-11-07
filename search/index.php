4f619fd673ca63d875175aaa4aa1a6ab849a13c3
TUAN PHAM <tuanp1711@gmail.com>
2014-11-07 00:01:01 -0500

    complete search controller

--- search/index.php
+++ search/index.php
@@ -3,12 +3,14 @@ $page_title = "Kết Quả Tìm Kiếm | Pure White";
 require_once('../inc/initialize.php');
 
 //Controller Search Form 
+// if a non-blank search term is specified in
+// the query string, perform a search
 $search_term = "";
 if (isset($_GET["s"])) {
   $search_term = trim($_GET["s"]);
   if ($search_term != "") {
-    require_once(ROOT_PATH . 'inc/popular_products_inc.php');
-    $search_results = get_search_products($search_term);
+    require_once(ROOT_PATH . 'inc/products.php');
+    $products = get_search_products($search_term);
   }
 } 
 
@@ -24,11 +26,27 @@ include(ROOT_PATH . 'inc/header.php');
 			<h3 class="panel-title"><img src="<?php echo BASE_URL; ?>img/search.png" alt="search-icon">Kết Quả Tìm Kiếm</h3>
 		</div>
 		<div class="panel-body">
-			<?php
-				if ($search_term != "") {
-					
-				} 
-			?>
+			<?php if ($search_term != "") :?>
+				<?php // if there are products found that match the search term, display them; ?>
+				<?php // otherwise, display a message that none were found ?>
+				<?php if (!empty($products)) : ?>
+					<?php
+						foreach ($products as $product) {
+							echo display_search_results($product); 	
+						} 
+					?>
+				<?php else :?>
+					<div class="alert alert-danger" role="alert">
+  					<p>Sản phẩm quý khách cần tìm không có trong cửa hàng. Vui lòng thử lại hoặc kiểm tra lại tên sản phẩm quý khách đang cần.</p>
+  					<a href="<?php echo BASE_URL; ?>" class="alert-link">Trở về trang chủ</a>
+					</div>
+				<?php endif; ?>
+			<?php else :?>
+				<div class="alert alert-danger" role="alert">
+  				<p>Quý khách vui lòng nhập tên sản phẩm cần tìm.</p>
+  				<a href="<?php echo BASE_URL; ?>" class="alert-link">Trở về trang chủ</a>
+				</div>
+			<?php endif; ?>
 		</div>
 	</div>
 </div>
