0e68811bc46c23bacc0a71028a9fd9b29ff664e6
TUAN PHAM <tuanp1711@gmail.com>
2014-11-06 21:28:36 -0500

    change pop to product

--- popular_products/popular_products.php
+++ popular_products/popular_products.php
@@ -2,33 +2,41 @@
 // Controller
 require_once('../inc/initialize.php');
 include(ROOT_PATH . 'inc/popular_products_inc.php'); 
-$pop_products_page = get_all_pop_products();
+
 
 
 // Pagination Controller 
+// retrieve current page number from query string; set to 1 if blank
+
 if (empty($_GET["pg"])) {
 	$current_page = 1;
 } else {
 	$current_page = $_GET["pg"];
 }
 
+// set strings like "frog" to 0; remove decimals
 $current_page = intval($current_page); //check the current page is integer if not convert to int 
 $total_products = get_products_count();
 $products_per_page = 8;
 $total_pages = ceil($total_products/$products_per_page); // ceil: round up to prevent odd numbers
+
+// redirect too-large page numbers to the last page
 if ($current_page > $total_pages) {
 	header("Location: ./pg=" . $total_pages);
 }
+// redirect too-small page numbers to the first page
 if ($current_page < 1) {
 	header("Location: ./");
 }
 
+// determine the start and end product for the current page
+// for example: in page 3 with 8 products, $start and $end would be 17 and 24
 $start = (($current_page - 1) * $products_per_page) + 1;
 $end = $current_page * $products_per_page;
 if ($end > $total_products) {
 	$end = $total_products;
 }
-$products = get_products_subset($start, $end);
+$products = get_products_subset($start,$end);
 
 
 
@@ -46,7 +54,7 @@ include(ROOT_PATH . 'inc/header.php');
 		<div class="panel panel-success panel-custom">
 			<div class="panel-heading">
 				<h3 class="panel-title">
-					<img src="<?php echo BASE_URL; ?>img/thumbs-up.png" alt="popular-products-logo">&nbsp;&nbsp;Sản Phẩm Nổi Bật  
+					<img src="<?php echo BASE_URL; ?>img/thumbs-up.png" alt="popular-products-logo">&nbsp;&nbsp;Sản Phẩm Nổi Bật
         </h3>
 			</div>
 
@@ -54,12 +62,13 @@ include(ROOT_PATH . 'inc/header.php');
 				<div class="row">
 					<?php 					
             $display_products_pop_page = "";
-						foreach ($pop_products_page as $pop_product) {           
-							$display_products_pop_page = display_products_pop_page($pop_product).$display_products_pop_page;	
+						foreach ($products as $product) {           
+							$display_products_pop_page = display_products_pop_page($product).$display_products_pop_page;	
   					} 
   					echo $display_products_pop_page;
   				?>
 				</div>
+			<?php include(ROOT_PATH . 'inc/partial_pagination.html.php'); ?>
 			</div>
 		</div>
 	</div>
