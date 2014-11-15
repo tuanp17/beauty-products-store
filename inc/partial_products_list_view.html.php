7c59a0e169df2486ca7868eb7d9f5110afcbc7da
TUAN PHAM <tuanp1711@gmail.com>
2014-11-14 23:49:06 -0500

    change thumbnail to list

--- inc/partial_products_list_view.html.php
+++ inc/partial_products_list_view.html.php
@@ -1,15 +1,7 @@
-<div class="col-sm-4 col-md-3">
-	<div class="thumbnail">
-		<img data-src="holder.js/250x250" src="<?php echo BASE_URL . $product["img"]; ?>" alt="<?php echo BASE_URL . $product["name"]; ?>">
-		<div class="caption">
-			<p>
-				<a href="<?php echo BASE_URL?>popular_products/<?php echo $product["sku"]; ?>/" class="thumbnail-caption">
-					<strong><?php echo $product["name"]; ?></strong>
-				</a>
-			</p>
-			<p>';
-				<a href="<?php echo BASE_URL?>popular_products/<?php echo $product["sku"]; ?>/" class="btn btn-success" role="button">Xem chi tiết</a>
-			</p>
-		</div>
-	</div>
-</div>
+<li>
+	<a href="<?php echo BASE_URL?>popular_products/<?php echo $product["sku"]; ?>/">
+		<img src="<?php echo BASE_URL . $product["img"]; ?>" alt="<?php echo BASE_URL . $product["name"]; ?>">
+		<p><strong><?php echo $product["name"]; ?></strong></p>
+		<button class="btn btn-success" role="button">Xem chi tiết</button>
+	</a>
+</li>
